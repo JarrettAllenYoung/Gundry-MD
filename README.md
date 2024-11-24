ca242aef9adfab89f356e29df1ca32ed74d9fba1
Jarrett Allen Young <5515581+JarrettAllenYoung@users.noreply.github.com>
2024-11-23 19:17:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gundry-MD
+ 
---
+++ js/close X button.js
@@ -0,0 +1,24 @@
+// working code for sticky banners (at least the headers work)
+
+    jQuery(document).ready(function($){ // header
+			$('.close-banner-blue-v1').click(function(){
+				$('.promo-banner-blue-v1').hide();
+			});
+		});
+	
+	jQuery(document).ready(function($){ // header
+			$('.close-banner-green-v1').click(function(){
+				$('.promo-banner-green-v1').hide();
+			});
+		});
+	
+	jQuery(document).ready(function($){ // footer
+			$('.close-banner-blue-v2').click(function(){
+				$('.promo-banner-blue-v2').hide();
+			});
+		});
+	jQuery(document).ready(function($){ // footer
+			$('.close-banner-green-v2').click(function(){
+				$('.promo-banner-green-v2').hide();
+			});
+		});
