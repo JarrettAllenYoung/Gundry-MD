3cf39f0cd8feb4d8a8696e65ce1f157f63f3b40b
Jarrett Allen Young <young.jarrett@gmail.com>
2025-08-27 17:02:52 -0700

    update

--- VWO/VWO Move header, breadcrumbs, stars, reviews above carousel/source.js
+++ VWO/VWO Move header, breadcrumbs, stars, reviews above carousel/source.js
@@ -34,6 +34,36 @@ if(screen.width < 1024) {
     });
 }
 
+/* CUSTOM CODE (NEW) */
+vwo_$(".gh-unicorn-breadcrumbs-desktop").each(function () {
+	var destP = vwo_$("#top > div:nth-of-type(1) > div:nth-of-type(2)"),
+		dest = vwo_$("#top > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(4)");
+	if (dest.length)
+		dest.append(this);
+	else
+		destP.append(this);
+});
+if(screen.width < 1024) {
+    /* REARRANGE2 */
+    vwo_$(".gh-unicorn-breadcrumbs-mobile").each(function () {
+        var destP = vwo_$(".elementor-element-378b155"),
+            dest = vwo_$(".elementor-widget-powerreviews-stars");
+        if (dest.length)
+            dest.append(this);
+        else
+            destP.before(this);
+    });
+    /* CUSTOM CODE */
+    vwo_$(".elementor-element-378b155").each(function () {
+        var dest = vwo_$(".elementor-widget-powerreviews-stars"),
+            destP = vwo_$(".elementor-widget-offer-selector");
+        if (dest.length)
+            dest.append(this);
+        else
+            destP.before(this);
+    });
+}
+
 
 // CSS
 
@@ -61,3 +91,49 @@ if(screen.width < 1024) {
         float: unset !important;
     }
 }
+
+
+
+
+
+/// GPT
+vwo_$(function () {
+    function moveElement($element, $dest, $destP) {
+        if ($element.length && ($dest.length || $destP.length)) {
+            if ($dest.length) {
+                $dest.append($element);
+            } else if ($destP.length) {
+                $destP.before($element);
+            }
+        }
+    }
+    function runRearrange() {
+        // Desktop / default layout
+        moveElement(
+            vwo_$(".woocommerce-breadcrumb"),
+            vwo_$("#top > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(4)"),
+            vwo_$("#top > div:nth-of-type(1) > div:nth-of-type(2)")
+        );
+        // Mobile layout
+        if (window.innerWidth < 767) {
+            const $breadcrumb = vwo_$(".woocommerce-breadcrumb");
+            const $stars = vwo_$(".elementor-widget-powerreviews-stars");
+            const $breadcrumbParent = vwo_$(".elementor-element-e6386a9");
+            const $starsParent = vwo_$(".elementor-widget-offer-selector");
+            if ($breadcrumb.length && $stars.length) {
+                $breadcrumbParent.before($breadcrumb);
+                $breadcrumb.after($stars);
+            } else {
+                moveElement($breadcrumb, $breadcrumbParent, vwo_$(".elementor-widget-offer-selector"));
+                moveElement($stars, $starsParent, $breadcrumbParent);
+            }
+        }
+    }
+    // Keep checking until both breadcrumb & stars are in DOM
+    var checkInterval = setInterval(function () {
+        if (vwo_$(".woocommerce-breadcrumb").length && vwo_$(".elementor-widget-powerreviews-stars").length) {
+            clearInterval(checkInterval);
+            runRearrange();
+        }
+    }, 200);
+});
